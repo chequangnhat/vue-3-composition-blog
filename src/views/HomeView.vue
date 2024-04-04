799eb1670aae5d71ce808e353e541c2bafec9c26
dact-quangnhat <nhat.che@dac-datatech.vn>
2024-04-04 16:30:19 +0700

    some change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,23 @@ onMounted(() => {
 </script>
 
 <template>
-  <div v-for="(item, index) in useBlog.blogs" :key="index">
-    <CardItem v-if="index < 4" :blog="item" />
+  <div class="home-container ">
+    <div class="home-header">
+      <div>Readit.</div>
+      <div>
+        <div>Home</div>
+        <div>Articles</div>
+        <div>Team</div>
+        <div>Contact</div>
+      </div>
+    </div>
+    <div>
+      <span>Hello! Welcome to</span>
+      <span>Readit blog</span>
+      <span>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</span>
+    </div>
+    <div v-for="(item, index) in useBlog.blogs" :key="index">
+      <CardItem v-if="index < 4" :blog="item" />
+    </div>
   </div>
 </template>
