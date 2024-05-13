6563e05edf9b6c92b51a3bd1e9e3923c5fab52fd
DESKTOP-GVK516T\Dell <nhatche42@gmail.com>
2024-05-14 06:54:50 +0700

    homepage

--- src/components/Header/HeaderComponent.vue
+++ src/components/Header/HeaderComponent.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home-header xl:max-w-[80%] flex justify-between items-center mx-auto">
-    <div class="font-black text-2xl">
+  <div class="w-full home-header xl:max-w-[62%] flex justify-between items-center mx-auto mt-10">
+    <div class="font-black text-[26px]">
       <span class="text-white">Read</span><span class="text-[#FFD369]">it</span
       ><span class="text-white">.</span>
     </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import CardItem from '@/components/Card/CardItem.vue'
+// import CardItem from '@/components/Card/CardItem.vue'
 import HeaderComponent from '@/components/Header/HeaderComponent.vue'
 import { useBlogStore } from '@/stores/blog'
 import { onMounted } from 'vue'
@@ -11,20 +11,29 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="home-container w-full h-screen bg-gradient-to-tr from-[#F437F8] to-[#FABD67] pt-10">
+  <div class="home-container font-poppins w-sreen h-screen bg-gradient-to-tr from-[#F43BF3] via-[#F77EAC] to-[#FAC064] flex flex-col">
+
     <HeaderComponent />
-    <div>
-      <span>Hello! Welcome to</span>
-      <span>Readit blog</span>
-      <span
+  
+    <div class="h-4/6 w-5/6 xl:max-w-[60%] mx-auto flex flex-col text-white mt-36">
+      <span class="text-xl font-normal">Hello! Welcome to</span>
+      <span class="text-[12rem] font-bold">Readit blog</span>
+      <span class="max-w-[65%] text-lg font-normal"
         >Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
         there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the
         Semantics, a large language ocean.</span
       >
     </div>
-    <div v-for="(item, index) in useBlog.blogs" :key="index">
+<div>
+    <Icon icon="meteocons:pressure-low-fill" class="text-white text-2xl" />
+    <Icon icon="line-md:chevron-small-triple-down" class="text-white text-3xl" />
+<Icon icon="meteocons:pressure-low" width="100" height="100" color="white" style="color: white;"/>
+
+</div>
+
+    <!-- <div v-for="(item, index) in useBlog.blogs" :key="index">
       <CardItem v-if="index < 4" :blog="item" />
-    </div>
+    </div> -->
   </div>
-  đ
+fff
 </template>
