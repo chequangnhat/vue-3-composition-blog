4c6f9a8d1784f23daf2b85182f2984e627787d4a
DESKTOP-GVK516T\Dell <nhatche42@gmail.com>
2024-05-14 23:53:07 +0700

    add animated icon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 // import CardItem from '@/components/Card/CardItem.vue'
 import HeaderComponent from '@/components/Header/HeaderComponent.vue'
+import PressureLowFill from '@/components/icons/PressureLowFill.vue'
 import { useBlogStore } from '@/stores/blog'
 import { onMounted } from 'vue'
 const useBlog = useBlogStore()
@@ -11,10 +12,11 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="home-container font-poppins w-sreen h-screen bg-gradient-to-tr from-[#F43BF3] via-[#F77EAC] to-[#FAC064] flex flex-col">
-
+  <div
+    class="home-container font-poppins w-sreen h-screen bg-gradient-to-tr from-[#F43BF3] via-[#F77EAC] to-[#FAC064] flex flex-col"
+  >
     <HeaderComponent />
-  
+
     <div class="h-4/6 w-5/6 xl:max-w-[60%] mx-auto flex flex-col text-white mt-36">
       <span class="text-xl font-normal">Hello! Welcome to</span>
       <span class="text-[12rem] font-bold">Readit blog</span>
@@ -23,17 +25,14 @@ onMounted(() => {
         there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the
         Semantics, a large language ocean.</span
       >
+      <div>
+        <PressureLowFill />
+      </div>
     </div>
-<div>
-    <Icon icon="meteocons:pressure-low-fill" class="text-white text-2xl" />
-    <Icon icon="line-md:chevron-small-triple-down" class="text-white text-3xl" />
-<Icon icon="meteocons:pressure-low" width="100" height="100" color="white" style="color: white;"/>
-
-</div>
 
     <!-- <div v-for="(item, index) in useBlog.blogs" :key="index">
       <CardItem v-if="index < 4" :blog="item" />
     </div> -->
   </div>
-fff
+  fff
 </template>
