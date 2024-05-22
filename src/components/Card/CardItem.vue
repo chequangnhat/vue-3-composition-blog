d2e31b0e89970ba2fb9431d406b72cc75476b4b3
DESKTOP-GVK516T\Dell <nhatche42@gmail.com>
2024-05-22 23:34:36 +0700

    news list

--- src/components/Card/CardItem.vue
+++ src/components/Card/CardItem.vue
@@ -7,6 +7,46 @@ defineProps<{
 </script>
 
 <template>
+  <div class="card-item group mb-12">
+    <a href="blog/1" class="flex justify-center hover:bg-white">
+      <img
+        src="https://images.unsplash.com/photo-1715942163404-b44e0808536b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+        class="w-3/6 h-96 object-cover"
+      />
+      <div class="flex flex-col justify-between px-4 ml-6 leading-normal w-2/6">
+        <span class="topic">ILLUSTRATION</span>
+        <h3 class="text-[3rem] font-extrabold text-black-700 text-left">
+          Build a website in minutes with Adobe Templates
+        </h3>
+        <div class="h-9">
+          <div class="group-hover:block hidden">
+            <div class="icon-list w-40 flex justify-between">
+              <div
+                class="w-12 h-12 bg-gray-300 hover:bg-orange-200 flex justify-center items-center rounded-lg"
+              >
+                <Icon icon="uil:twitter" width="25" height="25" />
+              </div>
+              <div
+                class="w-12 h-12 bg-gray-300 hover:bg-orange-200 flex justify-center items-center rounded-lg"
+              >
+                <Icon icon="ri:facebook-fill" width="32" height="32" />
+              </div>
+              <div
+                class="w-12 h-12 bg-gray-300 hover:bg-orange-200 flex justify-center items-center rounded-lg"
+              >
+                <Icon icon="iconoir:instagram" width="32" height="32" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="publishDate">11/13/2019</div>
+      </div>
+    </a>
+  </div>
+</template>
+
+<!-- <template>
   <div class="w-4/5">
     <a href="blog/1" class="flex flex-col bg-white md:flex-row hover:bg-white">
       <img :src="blog.urlToImage" alt="" class="w-3/5 h-96" />
@@ -18,6 +58,6 @@ defineProps<{
       </div>
     </a>
   </div>
-</template>
+</template> -->
 
 <style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-// import CardItem from '@/components/Card/CardItem.vue'
+import CardItem from '@/components/Card/CardItem.vue'
 import HeaderComponent from '@/components/Header/HeaderComponent.vue'
 import PressureLowFill from '@/components/icons/PressureLowFill.vue'
 import { useBlogStore } from '@/stores/blog'
@@ -19,7 +19,7 @@ onMounted(() => {
 
     <div class="h-4/6 w-5/6 xl:max-w-[60%] mx-auto flex flex-col text-white mt-36">
       <span class="text-xl font-normal">Hello! Welcome to</span>
-      <span class="text-[12rem] font-bold">Readit blog</span>
+      <span class="text-[10rem] font-bold">Readit blog</span>
       <span class="max-w-[65%] text-lg font-normal"
         >Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
         there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the
@@ -33,6 +33,11 @@ onMounted(() => {
     <!-- <div v-for="(item, index) in useBlog.blogs" :key="index">
       <CardItem v-if="index < 4" :blog="item" />
     </div> -->
+  </div>
+  <div class="card-container w-5/6 mx-auto mt-28">
+    <CardItem />
+    <CardItem />
+    <CardItem />
   </div>
   fff
 </template>
